0fc19634c155eac4465fab5fd0af751e6c36f103
山根　由紀 <yukiyamane@yamaneyukis-MacBook-Pro.local>
2019-01-24 09:39:50 +0800

    画像をDBに保存

--- create.php
+++ create.php
@@ -1,4 +1,10 @@
 <?php
+
+// var_dump($_FILES['img']['name']);die();
+
+
+
+
 date_default_timezone_set('Asia/Manila');
 // 投稿の保存
         // データベースに接続
@@ -9,11 +15,16 @@ date_default_timezone_set('Asia/Manila');
        // ユーザーが入力した内容を取得
         $nickname = htmlspecialchars($_POST['nickname']);
         $comment = htmlspecialchars($_POST['comment']);
+        $file_path = 'img/'. $_FILES['img']['name'];
         // var_dump($nickname,$comment);
 
+        // 画像
+        move_uploaded_file($_FILES['img']['tmp_name'],$file_path);
+        // var_dump(123);die();
+
         //　SQL書く
-          $sql = 'INSERT INTO posts (nickname, comment, created) VALUES (?,?,?)';
-          $data = [$nickname,$comment,date('Y-m-d H:i:s')];
+          $sql = 'INSERT INTO posts (nickname, comment, created, img) VALUES (?,?,?,?)';
+          $data = [$nickname, $comment, date('Y-m-d H:i:s'), $file_path];
           $stmt = $dbh->prepare($sql);
           $stmt->execute($data);
   }
